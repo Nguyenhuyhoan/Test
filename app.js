a579d0bdfdb0bcf8e82d7d5b63faaf1efe119a26
Thượng Quang <45959879+Nguyenhuyhoan@users.noreply.github.com>
2025-06-16 09:21:31 +0700

    Create app.js

---
+++ app.js
@@ -0,0 +1,29 @@
+Zalo
+url: https://raw.githubusercontent.com/Nguyenhuyhoan/Test/main/Hhhu
+
+Facebook
+url: https://sub4unlock.io/9ziKTjh.plist
+
+YouTube
+url: https://sub4unlock.io/9ziKT.plist
+
+Messenger
+url: https://sub4unlock.io/9ziKT.plist
+
+Lazada
+url: https://sub4unlock.io/9ziKT.plist
+
+Zalo
+url: https://raw.githubusercontent.com/Nguyenhuyhoan/Test/main/Hhhu
+
+Facebook
+url: https://sub4unlock.io/9ziKTjh.plist
+
+YouTube
+url: https://sub4unlock.io/9ziKT.plist
+
+Messenger
+url: https://sub4unlock.io/9ziKT.plist
+
+Lazada
+url: https://sub4unlock.io/9ziKT.plist
